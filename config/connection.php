686a287ecdf2073f8a37c8510eb96f768d1eb7a9
Muhyari <mmuhyarisaadahilmi@gmail.com>
2023-06-10 19:31:38 +0700

    feat: card fitur ditambahkan dan stylenya

--- config/connection.php
+++ config/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "surat");
+$conn = mysqli_connect("localhost", "root", "", "todolist_db");
 
 if($conn->connect_error){
     die("koneksi gagal: " . $conn->connect_error);
--- panel/dashboard.php
+++ panel/dashboard.php
@@ -1,3 +1,10 @@
+<?php
+include("../config/connection.php");
+$query = mysqli_query($conn, "SELECT * FROM todolist LIMIT 10");
+$result = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,6 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link rel="stylesheet" href="../style/dashboard.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
@@ -32,6 +40,30 @@
                 <h1>Dashboard</h1>
                 <a href="#">Keluar</a>
             </div>
+            <div class="todolist-content">
+                <div class="todolist-content-header">
+                    <h2>what do you want to do today?</h2>
+                </div>
+                <div class="todolist-content-body">
+                    <?php foreach ($result as $key => $r) : ?>
+                        <div class="card">
+                            <div class="card-head">
+                                <p><?php echo $r["title"] ?></p>
+                                <i class="fa fa-heart-o fa-2x " aria-hidden="true"></i>
+                            </div>
+                            <div class="card-body">
+                                <p><?php echo $r["Todo"] ?></p>
+                            </div>
+                            <div class="card-footer">
+                                <div>
+                                    <i class="fa fa-trash fa-2x" aria-hidden="true"></i>
+                                </div>
+
+                            </div>
+                        </div>
+                    <?php endforeach ?>
+                </div>
+            </div>
         </main>
     </div>
 
@@ -56,6 +88,49 @@
         }
 
     }
+
+    function isActive() {
+        this.classList.remove("fa-heart-o")
+        this.classList.add("fa-heart")
+        this.removeEventListener('click', isActive)
+        this.addEventListener('click', notActive)
+    }
+
+    function notActive() {
+        this.classList.remove("fa-heart")
+        this.classList.add("fa-heart-o")
+        this.removeEventListener('click', notActive)
+        this.addEventListener('click', isActive)
+    }
+
+    const icon = document.querySelectorAll(".fa-heart-o")
+    icon.forEach(card => card.addEventListener('click', isActive))
+    icon.forEach(card => card.addEventListener('mouse', notActive))
+
+    const carousel = document.querySelector(".todolist-content-body")
+
+    let isDragging = false,
+        startScrollLeft, startX
+    const dragging = (e) => {
+        if (!isDragging) return;
+        carousel.scrollLeft = startScrollLeft - (e.pageX - startX)
+
+    }
+    const dragStart = (e) => {
+        isDragging = true
+        carousel.classList.add("dragging")
+        startScrollLeft = carousel.scrollLeft
+        startX = e.pageX
+    }
+
+    const dragStop = () => {
+        isDragging = false
+        carousel.classList.remove("dragging")
+    }
+
+    carousel.addEventListener("mousemove", dragging)
+    carousel.addEventListener("mousedown", dragStart)
+    window.addEventListener("mouseup", dragStop)
 </script>
 
 </html>
